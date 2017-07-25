38e4697c0e86014e16557af78c4bc975f8c05777
shengyang <123456>
2017-07-25 09:57:33 +0800

    搭建框架

---
+++ src/Editor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="editor">
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'editor',
+    components: {},
+    mounted() {
+
+    }
+  }
+</script>
+
+<style>
+
+  #editor {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+</style>
