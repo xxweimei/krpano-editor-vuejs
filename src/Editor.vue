6c9d26203557ce6091907fa18e27800802413827
shengyang <123456>
2017-08-03 11:24:16 +0800

    编辑页开发

--- src/Editor.vue
+++ src/Editor.vue
@@ -6,28 +6,48 @@
           <span class="icon is-small"><i class="fa fa-floppy-o"></i></span>
           <span class="func-button-text">保存</span>
         </div>
+        <div class="line"></div>
         <div class="has-text-centered has-text-white func-button">
           <span class="icon is-small"><i class="fa fa-search"></i></span>
           <span class="func-button-text">预览</span>
         </div>
-        <div class="func-button" style="height: 100px"></div>
+        <div class="line"></div>
+        <div style="height: 100px"></div>
+        <div class="line"></div>
         <div class="has-text-centered has-text-white func-button"
              :class="{'func-selected' : module == 0}" @click="changeModule(0)">
           <span class="icon is-small"><i class="fa fa-window-restore"></i></span>
           <span class="func-button-text">场景</span>
         </div>
+        <div class="line"></div>
         <div class="has-text-centered has-text-white func-button"
              :class="{'func-selected' : module == 1}" @click="changeModule(1)">
           <span class="icon is-small"><i class="fa fa-eye"></i></span>
           <span class="func-button-text">视角</span>
         </div>
+        <div class="line"></div>
         <div class="has-text-centered has-text-white func-button"
              :class="{'func-selected' : module == 2}" @click="changeModule(2)">
           <span class="icon is-small"><i class="fa fa-dot-circle-o"></i></span>
           <span class="func-button-text">热点</span>
         </div>
+        <div class="line"></div>
       </div>
-      <div class="column is-10"></div>
+      <div class="column is-10">
+        <div class="scene-module has-text-white" v-show="module == 0">
+          <h5 class="title is-5 has-text-white scene-title">场景列表</h5>
+          <div class="line"></div>
+          <div class="scene-item">
+            <div>场景一场景一场景一场景一场景一场景一</div>
+            <figure class="image is-128x128">
+              <img src="../static/panos/1.tiles/thumb.jpg">
+            </figure>
+            <a class="button is-primary is-inverted is-outlined a-pencil"><span class="icon"><i class="fa fa-pencil"></i></span></a>
+            <a class="button is-primary is-inverted is-outlined a-home"><span class="icon"><i class="fa fa-home"></i></span></a>
+          </div>
+          <div class="line"></div>
+        </div>
+      </div>
     </div>
     <div class="camera" v-show="module == 1">
       <div class="camera-top"></div>
@@ -67,7 +87,7 @@
         this.krpano.set("layer[skin_splitter_bottom].visible", false);
         this.krpano.set("layer[skin_scroll_window].visible", false);
       },
-      changeModule(module){
+      changeModule(module) {
         this.module = module
       }
     }
@@ -80,25 +100,30 @@
     width: 100%;
   }
 
-  .func-button {
-    cursor: pointer;
-    height: 50px;
-    border-bottom: solid 1px black;
-    border-top: solid 1px #4b4b4b;
-    .func-button-text {
-      font-size: 5px;
-    }
-    span {
-      width: 100%;
-    }
+  .line {
+    width: 100%;
+    height: 0;
+    border-top: solid black 1px;
+    border-bottom: solid 1px #4b4b4b;
   }
 
   .func-div {
     border-right: solid 1px black;
-  }
 
-  .func-selected {
-    background-color: #427afb;
+    .func-button {
+      cursor: pointer;
+      height: 50px;
+      .func-button-text {
+        font-size: 5px;
+      }
+      span {
+        width: 100%;
+      }
+    }
+
+    .func-selected {
+      background-color: #427afb;
+    }
   }
 
   .camera {
@@ -215,6 +240,45 @@
       border: white 2px solid;
       bottom: 25%;
       right: 38%;
+    }
+  }
+
+  .scene-module {
+    .scene-title {
+      line-height: 50px;
+      padding-left: 10px;
+      margin: 0;
+    }
+
+    .scene-item {
+      margin: 5px 15px 15px 15px;
+      height: 168px;
+      width: 168px;
+      div {
+        line-height: 40px;
+        height: 40px;
+        width: 168px;
+      }
+      a {
+        border: 0;
+        position: relative;
+        height: 64px;
+        width: 40px;
+        background-color: #3a3a3a;
+        border-radius: 0;
+        &:hover {
+          background-color: #427afb !important;
+          color: white !important;
+        }
+      }
+      .a-pencil {
+        left:128px;
+        bottom: 128px;
+      }
+      .a-home {
+        left:84px;
+        bottom: 64px;
+      }
     }
   }
 
