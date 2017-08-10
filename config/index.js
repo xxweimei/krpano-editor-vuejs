c8a7642a99f64306cd20ce2d0bb78ef85212b362
xxweimei <xxweimei@gmail.com>
2017-08-10 09:45:00 +0800

    打包调整

--- config/index.js
+++ config/index.js
@@ -4,9 +4,9 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/krpano-vuejs.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'krpano-vuejs',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
